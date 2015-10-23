8e994854fa65f007afb804851fab9201adaa91cd
KelvinOjo <B00059571@Student.itb.ie>
2015-10-23 09:50:43 +0100

    Forth commit, Start of track build in unity under kelvin scene

---
+++ UltimateRunner/Assets/KelvinPlayGround/Script/PlayerControlls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlls : MonoBehaviour {
+
+
+	public float speed;
+	private Vector3 dir;
+
+	// Use this for initialization
+	void Start () 
+	{
+		dir = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			dir = Vector3.forward;
+		}
+
+		float move = speed * Time.deltaTime;
+		transform.Translate (dir * move);
+	
+	}
+}
